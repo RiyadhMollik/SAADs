f217705fd2bed7b290fb784f1b76a073dd40a561
Riyadh <106389622+Tabrid@users.noreply.github.com>
2025-05-04 08:14:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ import weatherRoutes from './routes/weatherRoutes.js';
 import statsRoutes from "./routes/statsRoutes.js";
 import sunshineRoutes from "./routes/sunshineRoutes.js";
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 app.use(bodyParser.json());
 app.use(cors());
